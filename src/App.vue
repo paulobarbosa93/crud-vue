844d20856789fceb64bde96e9ae2529fdb81ead3
Paulo Ney Barbosa Saturnino <paulo.n.barbosa@hotmail.com>
2016-08-29 19:59:28 -0300

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Cervejarias></Cervejarias>
+  </div>
+</template>
+
+<script>
+import Cervejarias from './Cervejarias.vue'
+
+export default {
+  components: {
+    Cervejarias
+  }
+}
+</script>
+
+<style>
+body {
+    margin-top: 50px
+}
+</style>
